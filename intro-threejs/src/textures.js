bc2b9b969a44214d37c34693c5fb4ef4ef9fbc24
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-01-10 17:24:13 +0100

    feat: add texture

--- intro-threejs/src/textures.js
+++ intro-threejs/src/textures.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 
 let camera, scene, renderer;
 let cube = null;
-let sphere = null;
-let plain = null;
+let textureBox = null;
+
 
 const init = () => {
   // Create a scene
@@ -29,36 +29,20 @@ const init = () => {
   renderer.setSize(sizes.width , sizes.height);
 }
 
+const loadTexture = async () => {
+  const loader = new THREE.TextureLoader();
+  textureBox = await loader.loadAsync('./../public/textures/box_texture.jpeg');
+  
+}
+
 const createCube = () => {
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   const material = new THREE.MeshBasicMaterial({ 
-    color: '#8e44ad',
+    map: textureBox,
    })
   cube = new THREE.Mesh(geometry, material);
   cube.position.x = -2;
   scene.add(cube);
-}
-
-const createSphere = () => {
-  const geometry = new THREE.SphereGeometry(0.75, 50, 50)
-  const material = new THREE.MeshBasicMaterial({ 
-    color: '#f1c40f',
-    wireframe: true,
-   })
-  sphere = new THREE.Mesh(geometry, material);
-  sphere.position.x = 2;
-  scene.add(sphere);
-}
-
-const createPlain = () => {
-  const geometry = new THREE.PlaneGeometry(2, 2)
-  const material = new THREE.MeshBasicMaterial({ 
-    color: '#e74c3c',
-   })
-  const plain = new THREE.Mesh(geometry, material);
-  plain.position.y = -2;
-  plain.rotation.x = -Math.PI * 0.5;
-  scene.add(plain);
 }
 
 
@@ -66,21 +50,15 @@ const animate = () => {
   requestAnimationFrame( animate );
   renderer.render( scene, camera );
 
-  if(cube != null){
+  if(cube) {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-  }
-
-  if(sphere != null){
-    sphere.rotation.x += 0.01;
-    sphere.rotation.y += 0.01;
   }
 }
 
 init();
+await loadTexture();
 createCube();
-createSphere();
-createPlain();
 animate();
 
 
